e3f5592f8d7cf6ccea13eac0bc15cac4f0f7b309
hakimxiao <alhakimabusolihin@gmail.com>
2026-08-10 23:28:16 +0700

    static route dan dynamic route serta contoh http method pada nest.js

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+// |##| static Route dan dynamic pada nest.js : * SELALU LETAKKAN STATIC ROUTE DI ATAS DYNAMIC AGAR TIDAK MEMAKAN ROUTE STATIC
+// @Get('all')          // GET /user/all
+// @Get(':id)           // GET /user/:id - dynamic segment
+// @Post()              // POST /user
+// @Put(':id')          // PUT /user/:id
+// @Delete(':id')       // DELETE /user/:id
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  // GET /user
+  @Get()
+  getUser() {
+    return [
+      { id: 1, name: 'John Doe' },
+      { id: 1, name: 'Hakim' },
+    ];
+  }
+}
